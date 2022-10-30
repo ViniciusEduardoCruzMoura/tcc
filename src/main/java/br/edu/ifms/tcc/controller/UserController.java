02efbc5bc0b89bc1e900917acc0d8ef556d1b7aa
CruzMoura <contato.cruz.moura@gmail.com>
2022-10-29 21:36:25 -0400

    adicionado a funcionalidade de deletar o usuario registrado no banco de dados

--- src/main/java/br/edu/ifms/tcc/controller/UserController.java
+++ src/main/java/br/edu/ifms/tcc/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -40,6 +41,13 @@ public class UserController {
     public String listUsers(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "/auth/admin/admin-list-user";
+    }
+
+    @GetMapping("/admin/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Id invalido: " + id));
+        userRepository.delete(user);
+        return "redirect:/user/admin/list";
     }
 
 }
